a7da21c8cdafac2bfb6f490c76ce465fdbb2340d
AhmedElaidy <alaidy.y123@gmail.com>
2024-01-22 15:40:53 +0200

    ui enhancments

--- components/ProductTemplate/ProductTemplate.js
+++ components/ProductTemplate/ProductTemplate.js
@@ -39,13 +39,12 @@ const ProductTemplate = (props) => {
           {type && type == "top_deals" ? (
             <div
               className={` bg-orange-500 text-white sm:pl-1 flex py-1 pl-1 pr-1 md:pl-3 md:pr-5  rounded-br-lg justify-center`}
+              style={{ paddingRight: "2px" }}
             >
               <span>{type_trans}</span>
             </div>
           ) : (
-            <div
-              className={` bg-blue-400 text-white py-1 px-5 rounded-br-lg`}
-            >
+            <div className={` bg-blue-400 text-white py-1 px-5 rounded-br-lg`}>
               <span>{type_trans}</span>
             </div>
           )}
